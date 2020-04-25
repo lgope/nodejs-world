765bc4b801ea60e9792061add7fe24d2ee27fa6f
lgope <ilakshmangope@gmail.com>
2020-04-25 22:15:12 +0600

    added message, deshboard, welcome page and user Model..

--- node-passport-login/app.js
+++ node-passport-login/app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 
 const passport = require('./config/passportConfig');
 const userRouter = require('./routes/userRoutes');
+const indexRouter = require('./routes/indexRoutes');
 
 const app = express();
 
@@ -14,8 +15,35 @@ const app = express();
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', indexRouter);
 app.use('/users', userRouter);
-
-
 
 module.exports = app;
