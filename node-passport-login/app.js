ef78879fa5d92d47157e31f2161fdf0998f0c16a
lgope <ilakshmangope@gmail.com>
2020-04-25 21:24:19 +0600

    added passport and installed npm packages 🛠

--- node-passport-login/app.js
+++ node-passport-login/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
+const passport = require('./config/passportConfig');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use('/', userRouter);
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use('/users', userRouter);
 
 
 
